2328bdec8f57170d98618e8b9d3d9e62354fdbf3
gycxkani <454683496@qq.com>
2025-03-08 21:21:12 +0800

    feat(class): 添加类的高级特性和访问修饰符
    - 公有、私有和受保护的访问修饰符
    - readonly修饰符和参数属性
    - 存取器的使用
    - 静态属性和方法
    - 抽象类的定义和使用

--- src/03-class.js
+++ src/03-class.js
@@ -114,4 +114,138 @@ var __extends = (this && this.__extends) || (function () {
     // 如果子类型有扩展的方法，不能让子类引用指向父类的实例
     // const dilu: Horse = new Animal2("的卢"); // 类型 "Animal2" 中缺少属性 "eat"，但类型 "Horse" 中需要该属性
     // dilu.run();
+    // 4. 公有、私有、受保护的访问修饰符
+    var Animal3 = /** @class */ (function () {
+        // 构造方法
+        function Animal3(name, age) {
+            this.name = name;
+            this.age = age;
+        }
+        // 公有方法
+        Animal3.prototype.eat = function () {
+            console.log("".concat(this.name, "\u5728\u5403\u4E1C\u897F"));
+        };
+        // 受保护方法
+        Animal3.prototype.sleep = function () {
+            console.log("".concat(this.name, "\u5728\u7761\u89C9"));
+        };
+        return Animal3;
+    }());
+    var Dog = /** @class */ (function (_super) {
+        __extends(Dog, _super);
+        function Dog(name, age) {
+            return _super.call(this, name, age) || this;
+        }
+        // 子类方法
+        Dog.prototype.run = function () {
+            console.log("".concat(this.name, "\u5728\u8DD1"));
+            // 调用受保护方法
+            this.sleep();
+        };
+        return Dog;
+    }(Animal3));
+    console.log(new Animal3("旺财", 32).name); // 旺财
+    // console.log(new Animal3("旺财", 32).age); // 属性“age”为私有属性，只能在类“Animal3”中访问
+    new Dog("旺财", 32).eat(); // 旺财在吃东西
+    new Dog("旺财", 32).run(); // 旺财在跑   旺财在睡觉
+    // 5.readonly修饰符和参数属性
+    var Person = /** @class */ (function () {
+        // readonly name: string = "张三";
+        // 使用参数属性，上面一行代码可 改为下面一行代码
+        function Person(name) {
+            if (name === void 0) { name = "张三"; }
+            this.name = name;
+            this.name = name;
+        }
+        return Person;
+    }());
+    var per = new Person();
+    console.log(per.name); // 张三
+    var per2 = new Person("李四");
+    console.log(per2.name); // 李四
+    // per.name = "王五"; // 无法为“name”赋值，因为它是只读属性
+    // console.log(per.name);
+    // 6. 存取器
+    // 不使用存取器
+    var Person1 = /** @class */ (function () {
+        function Person1() {
+        }
+        return Person1;
+    }());
+    var employee = new Person1();
+    employee.fullName = "Bob Smith";
+    if (employee.fullName) {
+        console.log(employee.fullName); // Bob Smith
+    }
+    // 使用存取器
+    var Person3 = /** @class */ (function () {
+        function Person3() {
+            this.firstName = "独孤";
+            this.lastName = "求败";
+        }
+        Object.defineProperty(Person3.prototype, "fullName", {
+            get: function () {
+                return this.firstName + "-" + this.lastName;
+            },
+            set: function (value) {
+                if (value) {
+                    var names = value.split("-");
+                    this.firstName = names[0];
+                    this.lastName = names[1];
+                }
+            },
+            enumerable: false,
+            configurable: true
+        });
+        return Person3;
+    }());
+    var p = new Person3();
+    console.log(p.fullName); // 独孤-求败
+    p.firstName = "独孤";
+    p.lastName = "天峰";
+    console.log(p.fullName); // 独孤-天峰
+    p.fullName = "逆天-唯我";
+    console.log(p.firstName, p.lastName); // 逆天 唯我
+    // 7. 静态属性
+    var Person4 = /** @class */ (function () {
+        function Person4() {
+            this.name = "独孤天峰"; // 实例属性--非静态属性
+        }
+        // 静态方法
+        Person4.getTitle = function () {
+            return Person4.title;
+        };
+        Person4.title = "我是静态属性";
+        return Person4;
+    }());
+    // 通过实例访问非静态属性
+    console.log(new Person4().name);
+    // 通过类名访问静态属性
+    console.log(Person4.title);
+    // 通过类名调用静态方法
+    console.log(Person4.getTitle());
+    // 8. 抽象类
+    var Animal4 = /** @class */ (function () {
+        function Animal4() {
+        }
+        // 普通方法
+        Animal4.prototype.run = function () {
+            console.log("动物在奔跑");
+        };
+        return Animal4;
+    }());
+    var Dog4 = /** @class */ (function (_super) {
+        __extends(Dog4, _super);
+        function Dog4() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        // 重写父类的抽象方法
+        Dog4.prototype.eat = function () {
+            console.log("狗在吃狗粮");
+        };
+        return Dog4;
+    }(Animal4));
+    var dog = new Dog4();
+    dog.eat();
+    dog.run();
 })();
--- src/03-class.ts
+++ src/03-class.ts
@@ -88,4 +88,124 @@
   // 如果子类型有扩展的方法，不能让子类引用指向父类的实例
   // const dilu: Horse = new Animal2("的卢"); // 类型 "Animal2" 中缺少属性 "eat"，但类型 "Horse" 中需要该属性
   // dilu.run();
+
+  // 4. 公有、私有、受保护的访问修饰符
+  class Animal3 {
+    // 公有属性
+    public name: string;
+    // 私有属性
+    private age: number;
+    // 构造方法
+    constructor(name: string, age: number) {
+      this.name = name;
+      this.age = age;
+    }
+    // 公有方法
+    public eat() {
+      console.log(`${this.name}在吃东西`);
+    }
+    // 受保护方法
+    protected sleep() {
+      console.log(`${this.name}在睡觉`);
+    }
+  }
+
+  class Dog extends Animal3 {
+    constructor(name: string, age: number) {
+      super(name, age);
+    }
+    // 子类方法
+    run() {
+      console.log(`${this.name}在跑`);
+      // 调用受保护方法
+      this.sleep();
+    }
+  }
+  console.log(new Animal3("旺财", 32).name); // 旺财
+  // console.log(new Animal3("旺财", 32).age); // 属性“age”为私有属性，只能在类“Animal3”中访问
+  new Dog("旺财", 32).eat(); // 旺财在吃东西
+  new Dog("旺财", 32).run(); // 旺财在跑   旺财在睡觉
+
+  // 5.readonly修饰符和参数属性
+  class Person {
+    // readonly name: string = "张三";
+    // 使用参数属性，上面一行代码可 改为下面一行代码
+    constructor(readonly name: string = "张三") {
+      this.name = name;
+    }
+  }
+  let per = new Person();
+  console.log(per.name); // 张三
+  let per2 = new Person("李四");
+  console.log(per2.name); // 李四
+  // per.name = "王五"; // 无法为“name”赋值，因为它是只读属性
+  // console.log(per.name);
+
+  // 6. 存取器
+  // 不使用存取器
+  class Person1 {
+    fullName?: string;
+  }
+  let employee = new Person1();
+  employee.fullName = "Bob Smith";
+  if (employee.fullName) {
+    console.log(employee.fullName); // Bob Smith
+  }
+  // 使用存取器
+  class Person3 {
+    firstName: string = "独孤";
+    lastName: string = "求败";
+    get fullName() {
+      return this.firstName + "-" + this.lastName;
+    }
+    set fullName(value) {
+      if (value) {
+        let names = value.split("-");
+        this.firstName = names[0];
+        this.lastName = names[1];
+      }
+    }
+  }
+  const p = new Person3();
+  console.log(p.fullName); // 独孤-求败
+  p.firstName = "独孤";
+  p.lastName = "天峰";
+  console.log(p.fullName); // 独孤-天峰
+  p.fullName = "逆天-唯我";
+  console.log(p.firstName, p.lastName); // 逆天 唯我
+
+  // 7. 静态属性
+  class Person4 {
+    name: string = "独孤天峰"; // 实例属性--非静态属性
+    static title: string = "我是静态属性";
+    // 静态方法
+    static getTitle() {
+      return Person4.title;
+    }
+  }
+  // 通过实例访问非静态属性
+  console.log(new Person4().name);
+  // 通过类名访问静态属性
+  console.log(Person4.title);
+  // 通过类名调用静态方法
+  console.log(Person4.getTitle());
+
+  // 8. 抽象类
+  abstract class Animal4 {
+    // 抽象方法
+    abstract eat(): void;
+    // 普通方法
+    run() {
+      console.log("动物在奔跑");
+    }
+  }
+  class Dog4 extends Animal4 {
+    // 重写父类的抽象方法
+    eat() {
+      console.log("狗在吃狗粮");
+    }
+  }
+  const dog = new Dog4();
+  dog.eat(); // 狗在吃狗粮
+  dog.run(); // 动物在奔跑
 })();
